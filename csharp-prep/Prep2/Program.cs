3f5e0c5e81d475e700e9470aa67f7705ab51a3b4
Jojocrazy-byui <hen21054@byui.edu>
2025-09-19 12:09:57 -0600

    to finish

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int score;
+        string letter_grade;
+        // Take in an integer for a grade, then return what the letter grade would be
+        Console.WriteLine("What is your total score?");
+        score = int.Parse(Console.ReadLine());
+
+        // Letter grade classification
+        if (score >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (score >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (score >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (score >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+
+        // Declaration of what our letter grade is
+        Console.WriteLine($"Your letter grade then is {letter_grade}");
     }
 }
