695b9b40658e75520a6361802677f35ab181bb9d
Jojocrazy-byui <hen21054@byui.edu>
2025-09-29 09:25:45 -0600

    Program 3-4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Random Class
+        var rand = new Random();
+
+        // Variable hub
+        int play = 1;
+        int magic_number = rand.Next(99 + 1);
+        int guess = 0;
+
+
+        Console.WriteLine("----Welcome to Your wonderful magician Dylan----");
+        // Program loop
+        while (play != 0)
+        {
+            // Get the players guess
+            Console.Write("What do you guess the number is? -> ");
+            try
+            {
+                guess = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error - {e}");
+            }
+
+            // Check the guess against the magic number
+            if (guess == magic_number)
+            {
+                play = 0;
+            }
+            else if (guess > magic_number)// Give a lower or higher
+            {
+                play = 1;
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                play = 1;
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You Got me! You guessed the right number.");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,85 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Variable hub
+        List<int> user_input = new List<int>();
+        int input = 1;
+        int sum = 0, avg = 0, max, pos = 0;
+
+
+        // Get a user generated list. End once a 0 is entered
+        Console.WriteLine("----Welcome to John's list summarizer----");
+        try
+        {
+            Console.Write("Enter in a number ->");
+            input = int.Parse(Console.ReadLine());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error - {e}");
+        }
+        while (input != 0)
+        {
+            user_input.Add(input);
+            try
+            {
+                Console.Write("Enter in a number ->");
+                input = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error - {e}");
+            }
+        }
+
+        // Generate the sum
+        foreach (int i in user_input)
+        {
+            sum += i;
+        }
+
+        // Generate the avg.
+        foreach (int i in user_input)
+        {
+            avg += i;
+        }
+        avg = avg / user_input.Count;
+
+        // Generate the Max number
+        max = user_input[0];
+        foreach (int i in user_input)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+
+        // Generate the largest positive number
+        foreach (int i in user_input)
+        {
+            if (i > pos)
+            {
+                pos = i;
+            }
+        }
+
+
+        foreach (int i in user_input)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine($"Sum: {sum}"); //Sum
+        Console.WriteLine($"Average: {avg}"); //Avg
+        Console.WriteLine($"Maximun: {max}"); //Max
+        Console.WriteLine($"Largest Positive number: {pos}"); // 
+        // Sort? Print the List
+        
+
     }
 }
