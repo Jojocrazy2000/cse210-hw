5574b3d51a546a8d64cc01808d83636f3541942f
Jojocrazy-byui <hen21054@byui.edu>
2025-10-13 08:03:57 -0600

    Prove Develope02 start

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,17 @@
+// Entry Class
+// Data: string response, prompt, date
+// Method: Display(), Prompt()
+class Entry
+{
+    private string response, prompt, date;
+
+    void Display()
+    {
+
+    }
+
+    void Prompt()
+    {
+        
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,23 @@
+// Journal Class
+// Data: entries List<Entry>, string filepath
+// Method: Save(), Load(), Display()
+class Journal
+{
+    private List<Entry> _entries = new List<Entry> { };
+    private string _filepath;
+
+    void Save()
+    {
+
+    }
+
+    void Load()
+    {
+
+    }
+
+    void Display()
+    {
+
+    }
+}
